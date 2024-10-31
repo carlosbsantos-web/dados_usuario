bf2a10dbea01db9147446efd527c534b53a8b419
Carlos <carlos@gamil.com>
2024-10-31 14:30:51 -0300

    Projeto Dados Usuário

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const button = document.querySelector('#submit')
+const dados = document.querySelector('#demo')
+
+
+button.addEventListener('click', (e) => {
+e.preventDefault()
+
+const nameValue = nameInput.value;
+const emailValue = emailInput.value;
+
+console.log(nameValue);
+console.log(emailValue);
+
+document.querySelector('#demo').innerHTML = 'Dados atualizados'
+document.querySelector('#demo').style.color = 'green'
+
+
+
+if(nameValue === '' || emailValue === '') {
+
+    alert('digite seus dados')
+}
+
+
+});
+
